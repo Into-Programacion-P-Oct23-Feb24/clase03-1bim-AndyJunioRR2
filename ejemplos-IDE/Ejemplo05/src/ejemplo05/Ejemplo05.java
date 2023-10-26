2eed0581f3d9df8e3aeaca6c1823b5b1eec64154
AndyJunioRR2 <andyjunior20@gmail.com>
2023-10-26 17:32:59 -0500

    Ejemplo06

--- ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
@@ -30,7 +30,7 @@ public class Ejemplo05 {
         System.out.println(10 % 3 );
         
         System.out.println("----------------");
-        System.out.println(Math.pow(10, 2));
+        System.out.println(Math.pow(10, 2)); // El metodo pow me permite elevar en potencia a un valor asignado
         
         System.out.println("----------------");
         System.out.println(Math.sqrt(16));
